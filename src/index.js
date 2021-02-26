e95366151514b2264f7a8c37bdea2d041d7ca7d9
Caleb James <calebsjames@gmail.com>
2021-02-25 19:08:09 -0600

    KandyKorn component

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Router>
+        <Kennel />
+      </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
